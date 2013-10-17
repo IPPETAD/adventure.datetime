2b6e8db2817544667edf4de03b28f3156683aa1e
Andrew Fontaine <afontaine@ualberta.ca>
2013-10-16 20:25:51 -0600

    Initial creation of project in IntelliJ
    I don't want to see no Eclipse crud in this, folks.

---
+++ src/ca/cmput301f13t03/adventure_datetime/ChooseYourOwnActivity.java
@@ -0,0 +1,15 @@
+package ca.cmput301f13t03.adventure_datetime;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChooseYourOwnActivity extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
