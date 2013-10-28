cb55eca40b138309ea9fd9e22c866413fdcbedaf
James Finlay <jtfinlay@ualberta.ca>
2013-10-28 11:27:25 -0600

    ListView in AuthorList working.

--- adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorList.java
+++ adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorList.java
@@ -23,17 +23,88 @@
 package ca.cmput301f13t03.adventure_datetime.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import ca.cmput301f13t03.adventure_datetime.R;
+import ca.cmput301f13t03.adventure_datetime.model.Story;
 
 /** Called when activity is first created */
 public class AuthorList extends Activity {
 	private static final String TAG = "AuthorList";
-	
+
+	private ListView _listView;
+	private RowArrayAdapter _adapter;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.browse_authored);
+
+		// TODO : Load shit from the model
+
+		_listView = (ListView) findViewById(R.id.list_view);
+		_listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				// TODO : Launch activity from item click
+			}
+		});
+
 	}
+
+	@Override
+	public void onResume() {
+		
+		Story[] stories = new Story[10];
+		for (int i=0; i<stories.length; i++)
+			stories[i] = new Story();
+		
+		_adapter = new RowArrayAdapter(this, R.layout.listviewitem, stories);
+		_listView.setAdapter(_adapter);
+		
+		
+		super.onResume();
+	}
+
+	private class RowArrayAdapter extends ArrayAdapter<Story> {
+
+		private Context context;
+		private int layoutResourceID;
+		private Story[] values;
+
+		public RowArrayAdapter(Context context, int layoutResourceID, Story[] values) {
+			super(context, layoutResourceID, values);
+
+			this.context = context;
+			this.layoutResourceID = layoutResourceID;
+			this.values = values;
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			View rowView = inflater.inflate(R.layout.listviewitem, parent, false);
+
+			ImageView thumbnail = (ImageView) rowView.findViewById(R.id.thumbnail);
+			TextView title = (TextView) rowView.findViewById(R.id.title);
+			TextView author = (TextView) rowView.findViewById(R.id.author);
+			TextView lastPlayed = (TextView) rowView.findViewById(R.id.datetime);
+
+			// TODO: fill out views from values[position]
+
+
+			return rowView;
+		}
+	}
+
 
 }
