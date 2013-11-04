738733b45d6a3431bfb79e78814e3ddbf3e2e7ee
James Finlay <jtfinlay@ualberta.ca>
2013-11-04 11:19:57 -0700

    Access to buttons

--- adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorEdit_Overview.java
+++ adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorEdit_Overview.java
@@ -28,14 +28,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class AuthorEdit_Overview extends Fragment{
+	
+	Button _btnAdd, _btnOther;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			ViewGroup container, Bundle savedInstanceState) {
 		
 		View rootView = inflater.inflate(R.layout.overview_edit, container, false);
+		
+		_btnAdd = (Button) rootView.findViewById(R.id.add);
+		_btnOther = (Button) rootView.findViewById(R.id.other);
 		
 		return rootView;
 	}
