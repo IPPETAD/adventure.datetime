9736d5294624c5b6c401cf98e7078c31dcad7440
Evan DeGraff <evandegraff@gmail.com>
2013-11-20 16:10:39 -0700

    Adding subscribe/unsubscribe for comments

--- adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/model/Interfaces/ICommentsListener.java
+++ adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/model/Interfaces/ICommentsListener.java
@@ -7,6 +7,6 @@ import ca.cmput301f13t03.adventure_datetime.model.Comment;
 
 public interface ICommentsListener {
 	
-	void OnCommentsChange(List<String> newComments);
+	void OnCommentsChange(List<Comment> newComments);
 
 }
