3bc03307a08e8f2b30e9e908ff435f6d51a48c11
James Finlay <jtfinlay@ualberta.ca>
2013-11-20 16:06:51 -0700

    Changin it up

--- adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/model/Interfaces/ICommentsListener.java
+++ adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/model/Interfaces/ICommentsListener.java
@@ -1,11 +1,12 @@
 package ca.cmput301f13t03.adventure_datetime.model.Interfaces;
 
+import java.util.List;
 import java.util.Map;
 
 import ca.cmput301f13t03.adventure_datetime.model.Comment;
 
 public interface ICommentsListener {
 	
-	void OnCommentsChange(Map<String, Comment> newComments);
+	void OnCommentsChange(List<String> newComments);
 
 }
