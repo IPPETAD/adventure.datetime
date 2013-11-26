69ff416ac7d6430ebcc3318fa259affb4691492a
Jesse Tucker <jesseDtucker@gmail.com>
2013-11-26 15:00:10 -0700

    UI for creating choices - nonfunctional

--- adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorEdit_Edit.java
+++ adventure.datetime/src/ca/cmput301f13t03/adventure_datetime/view/AuthorEdit_Edit.java
@@ -23,30 +23,27 @@
 package ca.cmput301f13t03.adventure_datetime.view;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import ca.cmput301f13t03.adventure_datetime.R;
 import ca.cmput301f13t03.adventure_datetime.model.Choice;
 import ca.cmput301f13t03.adventure_datetime.model.Story;
 import ca.cmput301f13t03.adventure_datetime.model.StoryFragment;
-import ca.cmput301f13t03.adventure_datetime.model.Interfaces.ICurrentFragmentListener;
 import ca.cmput301f13t03.adventure_datetime.serviceLocator.Locator;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -57,14 +54,17 @@ import android.widget.TextView;
  * @author James Finlay
  *
  */
-public class AuthorEdit_Edit extends Fragment {
-
+public class AuthorEdit_Edit extends Fragment implements OnClickListener
+{
+	private static final String TAG = "AuthorEdit_Edit";
+	
 	private View _rootView;
 	private StoryFragment _fragment;
 	private Story _story;
 	
-	private Button _media, _choices;
+	private Button _addChoiceBtn;
 	private EditText _content;
+	private ListView _choiceList = null;
 	
 	public void setFragment(StoryFragment sf) {
 		_fragment = sf;
@@ -82,29 +82,102 @@ public class AuthorEdit_Edit extends Fragment {
 		setUpView();
 		return _rootView;
 	}
-	private void setUpView() {
+	
+	private void setUpView() 
+	{
 		if (_rootView == null) return;
 		if (_fragment == null) return;
-		//if (_story == null) return;
 		
-
 		/** Layout items **/
-		_media = (Button) _rootView.findViewById(R.id.btn_addMedia);
-		_choices = (Button) _rootView.findViewById(R.id.choices);
+		_addChoiceBtn = (Button) _rootView.findViewById(R.id.addChoice_btn);
+		_choiceList = (ListView) _rootView.findViewById(R.id.choicesList);
 		_content = (EditText) _rootView.findViewById(R.id.content);
 		
 		_content.setText(_fragment.getStoryText());
-
-		/** Choices **/
-		final List<String> lchoices = new ArrayList<String>();
-		for (Choice choice : _fragment.getChoices())
-			lchoices.add(choice.getText());
+		
+		_addChoiceBtn.setOnClickListener(this);
 	
+		_choiceList.setClickable(false);
+		_choiceList.setAdapter(new ChoiceAdapter(this.getActivity(), R.layout.choice_item, _fragment.getChoices()));
 	}
-	public void saveFragment() {
+	
+	public void saveFragment() 
+	{
 		_fragment.setStoryText(_content.getText().toString());
 		_story.updateTimestamp();
 		Locator.getAuthorController().saveFragment(_fragment);
 		Locator.getAuthorController().saveStory(_story);
+	}
+	
+	public void onClick(View v) 
+	{
+		// TODO::JT create a new choice and add it to the fragment
+	}
+	
+	private class ChoiceAdapter extends ArrayAdapter<Choice>
+	{
+		private Context m_context;
+		private List<Choice> m_values;
+		private List<ChoiceButtonCallback> m_callbacks = new ArrayList<ChoiceButtonCallback>();
+		
+		public ChoiceAdapter(Context context, int resource, List<Choice> choices) 
+		{
+			super(context, resource, choices);
+
+			m_context = context;
+			m_values = choices;
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			LayoutInflater inflater = (LayoutInflater) m_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			View rowView = inflater.inflate(R.layout.choice_item, parent, false);
+
+			Choice item = m_values.get(position);
+			
+			/** Layout Items **/
+			ImageButton deleteBtn = (ImageButton)rowView.findViewById(R.id.choice_remove_btn);
+			ImageButton choiceBtn = (ImageButton)rowView.findViewById(R.id.choices_edit_btn);
+			TextView txt = (TextView)rowView.findViewById(R.id.choice_txt);
+			
+			ChoiceButtonCallback callback = new ChoiceButtonCallback(_fragment, item);	
+			m_callbacks.add(callback);
+			
+			txt.setText(item.getText());
+			deleteBtn.setOnClickListener(callback);
+			choiceBtn.setOnClickListener(callback);
+			
+			return rowView;
+		}
+		
+		private class ChoiceButtonCallback implements OnClickListener
+		{
+			private StoryFragment m_origin = null;
+			private Choice m_choice = null;
+			
+			public ChoiceButtonCallback(StoryFragment frag, Choice choice)
+			{
+				m_origin = frag;
+				m_choice = choice;
+			}
+			
+			public void onClick(View btn) 
+			{
+				if(btn.getId() == R.id.choice_remove_btn)
+				{
+					// TODO::JT
+				}
+				else if(btn.getId() == R.id.choices_edit_btn)
+				{
+					// TODO::JT
+				}
+				else
+				{
+					Log.e(TAG, "Somehow clicked on a button that shouldn't exist...?");
+				}
+			}
+		}
 	}
 }
